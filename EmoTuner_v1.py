d6c547e9d8dbdba4dc2f16bd3f63c023b73572f1
Hariesh Ramesh <harieshkai@gmail.com>
2024-12-25 21:18:54 +0530

    Update EmoTuner_v1.py

--- EmoTuner_v1.py
+++ EmoTuner_v1.py
@@ -3,9 +3,11 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 from googleapiclient.discovery import build
+import os
+
 
 # Load the emotion detection model
-model = tf.keras.models.load_model("D:/Emotion-Based-Music-Recommender-main/models/emotion_model.h5")
+model = tf.keras.models.load_model("./models/emotion_model.h5")
 
 # Preprocess the image for the emotion model
 def preprocess_image(image):
